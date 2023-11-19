3dc3194dcfac3f585c3fd04b690b602b30cc9148
Máté László Jobbágy <jobbagymatte@gmail.com>
2023-11-19 21:58:37 +0100

    Add project files.

---
+++ SlidingPuzzle_JobbagyMate/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace SlidingPuzzle_JobbagyMate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonSolution = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonNewGame = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.buttonSolution);
+            this.panel1.Controls.Add(this.buttonExit);
+            this.panel1.Controls.Add(this.buttonNewGame);
+            this.panel1.Controls.Add(this.buttonReset);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(628, 426);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(320, 15);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 252);
+            this.textBox1.TabIndex = 2;
+            // 
+            // buttonSolution
+            // 
+            this.buttonSolution.Location = new System.Drawing.Point(536, 187);
+            this.buttonSolution.Name = "buttonSolution";
+            this.buttonSolution.Size = new System.Drawing.Size(75, 23);
+            this.buttonSolution.TabIndex = 1;
+            this.buttonSolution.Text = "Solution";
+            this.buttonSolution.UseVisualStyleBackColor = true;
+            this.buttonSolution.Click += new System.EventHandler(this.buttonSolution_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(536, 245);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 1;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonNewGame
+            // 
+            this.buttonNewGame.Location = new System.Drawing.Point(536, 216);
+            this.buttonNewGame.Name = "buttonNewGame";
+            this.buttonNewGame.Size = new System.Drawing.Size(75, 23);
+            this.buttonNewGame.TabIndex = 1;
+            this.buttonNewGame.Text = "New Game";
+            this.buttonNewGame.UseVisualStyleBackColor = true;
+            this.buttonNewGame.Click += new System.EventHandler(this.buttonNewGame_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(536, 158);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 23);
+            this.buttonReset.TabIndex = 1;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(101, 187);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(80, 80);
+            this.button8.TabIndex = 0;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(101, 101);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(80, 80);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(187, 188);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(80, 80);
+            this.button9.TabIndex = 0;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(15, 187);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(80, 80);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(187, 15);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 80);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(187, 101);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(80, 80);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(15, 101);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 80);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(101, 15);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 80);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(15, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 80);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(652, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonSolution;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonNewGame;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ SlidingPuzzle_JobbagyMate/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SlidingPuzzle_JobbagyMate
+{
+    public partial class Form1 : Form
+    {
+        int Counter;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void EmptySpotChecker(Button Butt1, Button Butt2)
+        {
+            if (Butt2.Text == "")
+            {
+                Butt2.Text = Butt1.Text;
+                Butt1.Text = "";
+            }
+        }
+
+        public void SolutionChecker()
+        {
+            if(button1.Text == "1" && button2.Text == "2" && button3.Text == "3" && button4.Text == "4" && button5.Text == "5" && button6.Text == "6" && button7.Text == "7" && button8.Text == "8")
+            {
+                MessageBox.Show("Well done you are a winner", "Shuffle Game", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            Counter = Counter + 1;
+            textBox1.Text = "Number of clicks: " + Counter;
+        }
+
+        public void Shuffle()
+        {
+            int[] bnum = new int[9];
+            int i, j, rowchecker;
+            Boolean flag = false;
+
+            i = 1;
+            do
+            {
+                Random rnd = new Random();
+                rowchecker = Convert.ToInt32((rnd.Next(0, 8)) + 1);
+
+                for (j = 1; j <= i; j++)
+                {
+                    if (bnum[j] == rowchecker)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag == true)
+                {
+                    flag = false;
+                }
+                else
+                {
+                    bnum[i] = rowchecker;
+                    i = i + 1;
+                }
+            }
+            while (i <= 8);
+
+            button1.Text = Convert.ToString(bnum[1]);
+            button2.Text = Convert.ToString(bnum[2]);
+            button3.Text = Convert.ToString(bnum[3]);
+            button4.Text = Convert.ToString(bnum[4]);
+            button5.Text = Convert.ToString(bnum[5]);
+            button6.Text = Convert.ToString(bnum[6]);
+            button7.Text = Convert.ToString(bnum[7]);
+            button8.Text = Convert.ToString(bnum[8]);
+            button9.Text = "";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            EmptySpotChecker(button7, button4);
+            EmptySpotChecker(button7, button8);
+            SolutionChecker();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            EmptySpotChecker(button3, button2);
+            EmptySpotChecker(button3, button6);
+            SolutionChecker();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult iExit = MessageBox.Show("Do you want to exit?", "Shuffle Games", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (iExit == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            DialogResult iExit = MessageBox.Show("Do you want to exit?", "Shuffle Games", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (iExit == DialogResult.Yes)
+            {
+                Application.ExitThread();
+            }
+        }
+
+        private void buttonSolution_Click(object sender, EventArgs e)
+        {
+            button1.Text = Convert.ToString(1);
+            button2.Text = Convert.ToString(2);
+            button3.Text = Convert.ToString(3);
+            button4.Text = Convert.ToString(4);
+            button5.Text = Convert.ToString(5);
+            button6.Text = Convert.ToString(6);
+            button7.Text = Convert.ToString(7);
+            button8.Text = Convert.ToString(8);
+            button9.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Shuffle();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EmptySpotChecker(button1, button2);
+            EmptySpotChecker(button1, button4);
+            SolutionChecker();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            EmptySpotChecker(button2, button1);
+            EmptySpotChecker(button2, button3);
+            EmptySpotChecker(button2, button5);
+            SolutionChecker();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            EmptySpotChecker(button4, button1);
+            EmptySpotChecker(button4, button5);
+            EmptySpotChecker(button4, button7);
+            SolutionChecker();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            EmptySpotChecker(button6, button3);
+            EmptySpotChecker(button6, button5);
+            EmptySpotChecker(button6, button9);
+            SolutionChecker();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            EmptySpotChecker(button5, button2);
+            EmptySpotChecker(button5, button4);
+            EmptySpotChecker(button5, button6);
+            EmptySpotChecker(button5, button8);
+            SolutionChecker();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            EmptySpotChecker(button8, button5);
+            EmptySpotChecker(button8, button7);
+            EmptySpotChecker(button8, button9);
+            SolutionChecker();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            EmptySpotChecker(button9, button6);
+            EmptySpotChecker(button9, button8);
+            SolutionChecker();
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            Shuffle();
+        }
+
+        private void buttonNewGame_Click(object sender, EventArgs e)
+        {
+            Shuffle();
+            textBox1.Clear();
+
+            this.Refresh();
+            this.Hide();
+            Form1 NewGame = new Form1();
+            NewGame.Show();
+        }
+    }
+}
